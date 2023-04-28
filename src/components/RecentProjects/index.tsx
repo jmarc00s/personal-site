cd63bb90cc955623d16382899dbf535fdb7127b4
João Marcos <jmarcossferreira@hotmail.com>
2023-04-28 12:19:16 -0300

    refactor: changes to use BorderedSection component

--- src/components/RecentProjects/index.tsx
+++ src/components/RecentProjects/index.tsx
@@ -2,21 +2,18 @@ import { Repo } from '@/types';
 import React from 'react';
 import { RecentProjectItem } from './components/RecentProjectsItem';
 import { ProjectsIcon } from '../icons';
+import { BorderedSection } from '../BorderedSection';
 
 type RecentProjectsProps = {
   repos: Repo[];
 };
 
 export const RecentProjects = ({ repos }: RecentProjectsProps) => (
-  <section className="w-full border rounded-lg border-base-300 px-6 py-4 shadow-sm">
-    <div className="flex gap-4 items-center mb-8">
-      <ProjectsIcon />
-      <p className="font-bold text-sm">Recent projects</p>
-    </div>
+  <BorderedSection icon={<ProjectsIcon />} title="Recent projects">
     <ul className="flex-col mb-8 ">
       {repos.map((repo) => (
         <RecentProjectItem key={repo.id} repo={repo} />
       ))}
     </ul>
-  </section>
+  </BorderedSection>
 );
--- src/components/WorkExperience/index.tsx
+++ src/components/WorkExperience/index.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { WORK_EXPERIENCES } from './constant';
 import { WorkExperienceItem } from './components/WorkExperienceItem';
 import { WorkIcon } from '../icons';
+import { BorderedSection } from '../BorderedSection';
 
 export const WorkExperience = () => {
   return (
-    <section className="w-full border rounded-lg border-base-300 px-6 py-4 shadow-sm">
-      <div className="flex gap-4 items-center mb-8">
-        <WorkIcon />
-        <p className="font-bold text-sm">Work experiences</p>
-      </div>
+    <BorderedSection icon={<WorkIcon />} title="Work experiences">
       <ul className="flex flex-col mb-8">
         {WORK_EXPERIENCES.map((workExperience, index) => (
           <WorkExperienceItem key={index} workExperience={workExperience} />
         ))}
       </ul>
-    </section>
+    </BorderedSection>
   );
 };
