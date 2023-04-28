68d9e9e0aef85c272ba6c833791bdf1931f40c11
João Marcos <jmarcossferreira@hotmail.com>
2023-04-28 12:17:25 -0300

    feat: add bordered section component

---
+++ src/components/BorderedSection/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement, ReactNode } from 'react';
+
+type BorderedSectionProps = {
+  children: ReactNode;
+  title: string;
+  icon: ReactElement;
+};
+
+export const BorderedSection = ({
+  children,
+  title,
+  icon,
+}: BorderedSectionProps) => {
+  return (
+    <section className="w-full border rounded-lg border-base-300 px-6 py-4 shadow-sm h-full">
+      <div className="flex gap-4 items-center mb-8">
+        {icon}
+        <p className="font-bold text-sm">{title}</p>
+      </div>
+      {children}
+    </section>
+  );
+};
